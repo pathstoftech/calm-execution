899ef09a54a87833138f6750872f4f41cdf8992e
pathstoftech <pathsoftech@gmail.com>
2026-05-11 00:05:52 +0400

    C3-02 Implemented HomeAction contract

---
+++ app/src/main/java/com/example/a30daysofcalmexecution/core/ui/home/HomeAction.kt
@@ -0,0 +1,32 @@
+package com.example.a30daysofcalmexecution.core.ui.home
+
+import com.example.a30daysofcalmexecution.core.model.SectionKey
+import com.example.a30daysofcalmexecution.core.model.TipId
+
+sealed interface HomeAction {
+    data class SelectSection(
+        val section: SectionKey?
+    ) : HomeAction
+
+    data class OpenTip(
+        val tipId: TipId
+    ) : HomeAction
+
+    data class ToggleBookmark(
+        val tipId: TipId
+    ) : HomeAction
+
+    data class ToggleCompleted(
+        val tipId: TipId
+    ) : HomeAction
+
+    data class SelectExpandedDetail(
+        val tipId: TipId?
+    ) : HomeAction
+
+    data object OpenSettings : HomeAction
+
+    data object RetryLoad : HomeAction
+
+    data object DismissMessage : HomeAction
+}
